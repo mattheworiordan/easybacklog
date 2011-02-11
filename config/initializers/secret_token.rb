b99a0e0c26a9ff19374d3736a370b5a0326d9fb1
Matthew O'Riordan <matthew.oriordan@gmail.com>
2011-02-11 17:28:26 +0000

    Rails base start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ibacklog::Application.config.secret_token = '1767515e3dcb83aa26da6392e1ecc039d327bb7f26c158321c486b4e0fe13a1f1d8629df5ad2be76ef72b08680674f63cb9d38d8bcc3e43b7f975afb0bb4fcec'
