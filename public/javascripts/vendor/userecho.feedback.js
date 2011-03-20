678177d4b2c069dea4a10d7ae90730f4a42ef608
Matthew O'Riordan <matthew.oriordan@gmail.com>
2011-03-20 20:04:17 +0000

    User echo was causing an error so delayed until load

--- public/javascripts/vendor/userecho.feedback.js
+++ public/javascripts/vendor/userecho.feedback.js
@@ -13,8 +13,8 @@ tab_hover_color:'#FF9305',
 tab_top: '75%'
 };
 
-(function() {
-    var _ue = document.createElement('script'); _ue.type = 'text/javascript'; _ue.async = true;
-    _ue.src = ('https:' == document.location.protocol ? 'https://s3.amazonaws.com/' : 'http://') + 'cdn.userecho.com/js/widget-1.4.gz.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(_ue, s);
-  })();
+$(document).ready(function() {
+  var _ue = document.createElement('script'); _ue.type = 'text/javascript'; _ue.async = true;
+  _ue.src = ('https:' == document.location.protocol ? 'https://s3.amazonaws.com/' : 'http://') + 'cdn.userecho.com/js/widget-1.4.gz.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(_ue, s);
+});
