f1bb3e8d43799fa9a71842e3c576487c6f4ff490
Matthew O'Riordan <matthew.oriordan@gmail.com>
2011-03-21 23:58:11 +0000

    Improved vTip library to support live events

--- public/javascripts/vendor/vtip.js
+++ public/javascripts/vendor/vtip.js
@@ -7,7 +7,7 @@ this.vtip = function() {
     this.xOffset = -10; // x distance from mouse
     this.yOffset = 10; // y distance from mouse       
     
-    $(".vtip").unbind().hover(    
+    $(".vtip").unbind().live('mouseenter',
         function(e) {
             this.t = this.title;
             this.title = ''; 
@@ -15,15 +15,15 @@ this.vtip = function() {
             
             $('body').append( '<p id="vtip"><img id="vtipArrow" />' + this.t + '</p>' );
                         
-            $('p#vtip #vtipArrow').attr("src", 'images/vtip_arrow.png');
+            $('p#vtip #vtipArrow').attr("src", '/images/vtip_arrow.png');
             $('p#vtip').css("top", this.top+"px").css("left", this.left+"px").fadeIn("slow");
             
-        },
+        }).live('mouseleave',
         function() {
             this.title = this.t;
             $("p#vtip").fadeOut("slow").remove();
         }
-    ).mousemove(
+    ).live('mousemove',
         function(e) {
             this.top = (e.pageY + yOffset);
             this.left = (e.pageX + xOffset);
