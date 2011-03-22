08ca57d26ac7aea9926a24feb4fadeccc30319bd
Matthew O'Riordan <matthew.oriordan@gmail.com>
2011-03-22 09:48:40 +0000

    Jammit assets committed at Tuesday 22, 2011

--- app/reports/story_cards_report.rb
+++ app/reports/story_cards_report.rb
@@ -34,22 +34,14 @@ class StoryCardsReport
       card_width = (page_dimensions.width - page_dimensions.margin_x*2)/2
       card_height = (page_dimensions.height - page_dimensions.margin_y*2)/2
 
-      pdf.bounding_box box_offset(0.05, 0.95, @box_count, page_size), :width => card_width*0.95, :height => card_height * 0.95 do
-        pdf.text_box "Project: #{story.theme.backlog.name}",
-          :at => [pdf.bounds.width * 0.05, pdf.bounds.height * 0.9],
-          :width => pdf.bounds.width * 0.65,
-          :height => pdf.bounds.height * 0.1,
-          :size => 14
-        pdf.formatted_text_box [{:text => "Code:"},{:text => "#{story.theme.code}#{story.unique_id}", :styles => [:bold]}],
-          :align => :right, :size => 16,
-          :at => [pdf.bounds.width * 0.70, pdf.bounds.height * 0.9],
-          :width => pdf.bounds.width/4,
-          :height => pdf.bounds.height * 0.1
-        pdf.text_box "Theme: #{story.theme.name}",
-          :size => 15,
-          :at => [pdf.bounds.width * 0.05, pdf.bounds.height * 0.8],
-          :width => pdf.bounds.width,
-          :height => pdf.bounds.height * 0.1
+      # create the reverse page if on first box
+      pdf.start_new_page(document_options) if (@box_count.modulo(BOXES_PER_PAGE) == 0)
+
+      # let's work on the first page
+      pdf.go_to_page pdf.page_count-1
+
+      offset = box_offset(0.05, 0.95, @box_count, page_size)
+      standard_box_fields(story, pdf, offset, card_width*0.9, card_height*0.9) do
         pdf.formatted_text_box [
             {:text => "As a "}, {:text => "#{story.as_a}", :styles => [:bold]},
             {:text => "\nI want to "}, {:text => "#{story.i_want_to}", :styles => [:bold]},
@@ -59,12 +51,27 @@ class StoryCardsReport
           :width => pdf.bounds.width * 0.9,
           :height => pdf.bounds.height * 0.6,
           :size => 20
-        pdf.stroke do
-          pdf.rectangle pdf.bounds.top_left, pdf.bounds.width, pdf.bounds.height
-        end
+      end
+
+      # let's work on the first page
+      pdf.go_to_page pdf.page_count
+
+      offset = mirror_box_offset(0.05, 0.95, @box_count, page_size)
+      standard_box_fields(story, pdf, offset, card_width*0.9, card_height*0.9) do
+        acceptance_criteria = story.acceptance_criteria.each_with_index.map { |crit, index| "#{index + 1}. #{crit.criterion}" }
+        pdf.formatted_text_box [
+            {:text => "Acceptance Criteria\n", :styles => [:bold]},
+            {:text => acceptance_criteria.join("\n") }
+          ],
+          :at => [pdf.bounds.width * 0.05, pdf.bounds.height * 0.65],
+          :width => pdf.bounds.width * 0.9,
+          :height => pdf.bounds.height * 0.6,
+          :size => 14
       end
 
       @box_count = @box_count + 1
+
+      # create a new page as we are out of pages
       pdf.start_new_page(document_options) if (@box_count.modulo(BOXES_PER_PAGE) == 0)
     end
 
@@ -88,8 +95,46 @@ class StoryCardsReport
       [offset[0] + x.to_f * card_width, offset[1] + y.to_f * card_height]
     end
 
-    # HTML encode
-    def h(unescaped)
-      CGI::escapeHTML("#{unescaped}")
+    # like box_offset above
+    # however this method returns co-ordinates of the back page of a double sided page
+    # i.e. horizontally mirrored
+    # page 1 --- page 2
+    # [1][0]    [0][1]
+    # [3][2]    [3][2]
+    def mirror_box_offset(x, y, box_position, page_size)
+      new_box_position = case box_position.modulo(BOXES_PER_PAGE)
+      when 0 then 1
+      when 1 then 0
+      when 2 then 3
+      when 3 then 2
+      end
+      box_offset(x, y, new_box_position, page_size)
+    end
+
+    # set up the stsandard fields & bounding box for a card
+    def standard_box_fields(story, pdf, box_offset, box_width, box_height, &block)
+      pdf.bounding_box box_offset, :width => box_width, :height => box_height do
+        pdf.text_box "Project: #{story.theme.backlog.name}",
+          :at => [pdf.bounds.width * 0.05, pdf.bounds.height * 0.9],
+          :width => pdf.bounds.width * 0.65,
+          :height => pdf.bounds.height * 0.1,
+          :size => 14
+        pdf.formatted_text_box [{:text => "#{story.theme.code}#{story.unique_id}", :styles => [:bold]}],
+          :align => :right, :size => 16,
+          :at => [pdf.bounds.width * 0.70, pdf.bounds.height * 0.9],
+          :width => pdf.bounds.width/4,
+          :height => pdf.bounds.height * 0.1
+        pdf.text_box "Theme: #{story.theme.name}",
+          :size => 15,
+          :at => [pdf.bounds.width * 0.05, pdf.bounds.height * 0.8],
+          :width => pdf.bounds.width,
+          :height => pdf.bounds.height * 0.1
+
+        yield
+
+        pdf.stroke do
+          pdf.rectangle pdf.bounds.top_left, pdf.bounds.width, pdf.bounds.height
+        end
+      end
     end
 end
