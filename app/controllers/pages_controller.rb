49b8d3cd8f298fc2baa81f626d8bd53424569285
Matthew O'Riordan <matthew.oriordan@gmail.com>
2011-03-24 16:13:30 +0000

    Improved home page with sign up form & activity

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,8 @@ class PagesController < ApplicationController
     if user_signed_in? && session[:last_url].blank?
       redirect_to companies_path
     end
+    if user_signed_in?
+      @recent_backlogs = current_user.companies.map { |company| company.backlogs }.flatten.sort_by(&:updated_at).reverse[0..10]
+    end
   end
 end
