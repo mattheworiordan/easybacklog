16aaaf1b0efd555af4a45940becd535254a4e785
Matthew O'Riordan <matthew.oriordan@gmail.com>
2011-03-25 15:42:43 +0000

    IE8+ support

--- public/javascripts/models/backlog.js
+++ public/javascripts/models/backlog.js
@@ -1,4 +1,9 @@
 var Backlog = Backbone.Model.extend({
+  url: function() {
+    // url specified explicitly for this model as IE8 was assuming the URL was the same as page loaded for the AJAX request and caching it occassionally
+    return (this.collection.url() + (this.isNew() ? '' : '/' + this.id) + '?cache-buster' + Math.floor(Math.random()*1000000));
+  },
+
   // access to stories child collection
   Themes: function() {
     if (this._themes == null) {
