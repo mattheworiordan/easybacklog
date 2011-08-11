dff1d7f9462db0591d61591f77f58e4b56859781
Matthew O'Riordan <matthew.oriordan@gmail.com>
2011-08-11 13:48:01 +0100

    </script> fix in JSON which breaks app
    https://aque.basecamphq.com/projects/6147377-easybacklog-3141-1/posts/49869963/comments

--- app/helpers/backlogs_helper.rb
+++ app/helpers/backlogs_helper.rb
@@ -2,4 +2,9 @@ module BacklogsHelper
   def backlog_date(backlog)
     l backlog.created_at, :format => :long, :locale => backlog.company.locale.code.to_sym
   end
+
+  # if JSON is embedded in CDATA Javascript and there is a value of </script> it closes the opening <script> tag incorrectly
+  def embedded_json_safe(json)
+    json.gsub(/<\/script>/i,'<" + "/script>')
+  end
 end
