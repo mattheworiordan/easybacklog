be4563dd43f0d841840fe62607ea942c49464fd8
Matthew O'Riordan <matthew.oriordan@gmail.com>
2011-08-17 17:47:14 +0100

    Postgres compatibility for 5090 upgrade

--- db/migrate/20110816220610_add5090_default.rb
+++ db/migrate/20110816220610_add5090_default.rb
@@ -2,8 +2,8 @@ class Add5090Default < ActiveRecord::Migration
   def self.up
     add_column :companies, :default_use_50_90, :boolean
     add_column :backlogs, :use_50_90, :boolean
-    execute 'update backlogs set use_50_90 = 1'
-    execute 'update companies set default_use_50_90 = 1'
+    execute "update backlogs set use_50_90 = '1'"
+    execute "update companies set default_use_50_90 = '1'"
   end
 
   def self.down
