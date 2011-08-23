29d38f34a3286534ba127424713190b0e936c0bd
Matthew O'Riordan <matthew.oriordan@gmail.com>
2011-08-23 23:01:50 +0100

    JQuery Focus fix for tests

---
+++ public/javascripts/test/jquery.focus.js
@@ -0,0 +1,28 @@
+/**
+ * JQuery tries to use native CSS selectors instead of the Sizzle selector
+ * engine for performance reasons.
+ *
+ * This causes problems when trying to test intefaces using the
+ * :focus pseudo selector as unless the web page and browser window
+ * has the focus, all elements are considered to be without focus.
+ * Checking for :focus in Selenium or Capybara tests therefore fail if
+ * using JQuery or Sizzle.
+ *
+ * Sizzle will however return true for a :focus element even if the
+ * window itself has lost focus if we force it not use the native selector functions
+ * This script forces Sizzle to use its own engine over native selectors.
+ *
+ * This file MUST be included before JQuery or Sizzle is loaded
+ *
+ * Refer to http://blog.mattheworiordan.com/ for more info on this issue
+ *
+ **/
+
+/* Prevent use of native find selector */
+document.querySelectorAll = false;
+
+/* Prevent use of native matches selector */
+document.documentElement.matchesSelector = false;
+document.documentElement.mozMatchesSelector = false;
+document.documentElement.webkitMatchesSelector = false;
+document.documentElement.msMatchesSelector = false;
