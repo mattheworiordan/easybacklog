57d0b3d80728be782bf428330e51c65bdcbc6cb5
Matthew O'Riordan <matthew.oriordan@gmail.com>
2011-09-08 03:35:01 +0100

    Compass Heroku compatibility, moved CSS files into tmp

--- config/compass.rb
+++ config/compass.rb
@@ -7,6 +7,8 @@ project_type = :rails
 # Set this to the root of your project when deployed:
 http_path = "/"
 
+css_dir      = 'tmp/stylesheets'
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
