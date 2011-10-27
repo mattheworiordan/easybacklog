353a612983937697291535630796cdd438359f4f
Matthew O'Riordan <matthew.oriordan@gmail.com>
2011-10-27 22:47:49 +0100

    Acceptance criteria cannot be deleted when story done

--- app/models/acceptance_criterion.rb
+++ app/models/acceptance_criterion.rb
@@ -3,10 +3,23 @@ class AcceptanceCriterion < ActiveRecord::Base
 
   belongs_to :story
 
+  before_validation :check_story_editable
+  before_destroy :prevent_delete_if_story_not_editable
   attr_accessible :criterion, :position
+
+  include Snapshot
+  include ActiveRecordExceptions
 
   def editable?
     story.theme.backlog.editable?
   end
-  include Snapshot
+
+  private
+    def check_story_editable
+      errors.add :base, 'Cannot edit as story is marked as done and therefore locked' if story.done?
+    end
+
+    def prevent_delete_if_story_not_editable
+      raise AcceptanceCriterion::RecordLocked, 'Cannot delete as story is marked as done and therefore locked' if story.done?
+    end
 end
---
+++ spec/models/acceptance_criteria_spec.rb
@@ -0,0 +1,31 @@
+# encoding: UTF-8
+
+require 'spec_helper'
+
+describe AcceptanceCriterion do
+  # default sprint story status is needed when any story is assigned to a sprint
+  let!(:default_sprint_story_status) { Factory.create(:sprint_story_status, :status => 'To do', :code => SprintStoryStatus::DEFAULT_CODE) }
+  let!(:done_sprint_story_status) { Factory.create(:sprint_story_status, :status => 'Done', :code => SprintStoryStatus::DONE_CODE) }
+
+  before(:each) do
+    @criterion = Factory.create(:acceptance_criterion)
+    @story = @criterion.story
+    @sprint = Factory.create(:sprint, :backlog => @criterion.story.theme.backlog)
+    @sprint.stories << @story
+  end
+
+  it 'should not allow changes to acceptance criterion when a story is done' do
+    @story.sprint_story.sprint_story_status = done_sprint_story_status;
+    @story.sprint_story.save!
+
+    @criterion.reload
+    @criterion.criterion = 'new criterion'
+    expect { @criterion.save! }.should raise_error ActiveRecord::RecordInvalid, /Cannot edit as story is marked as done and therefore locked/
+
+    @criterion.reload
+    @criterion.position = 5
+    expect { @criterion.save! }.should raise_error ActiveRecord::RecordInvalid, /Cannot edit as story is marked as done and therefore locked/
+
+    expect { @criterion.destroy }.should raise_error AcceptanceCriterion::RecordLocked, /Cannot delete as story is marked as done and therefore locked/
+  end
+end
