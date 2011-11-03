1f7067a91abf414c0f05e609b767542ad1a783de
Matthew O'Riordan <matthew.oriordan@gmail.com>
2011-11-03 20:39:05 +0000

    Compare snapshots must show last modified date

--- app/helpers/backlogs_helper.rb
+++ app/helpers/backlogs_helper.rb
@@ -1,6 +1,6 @@
 module BacklogsHelper
   def backlog_date(backlog)
-    I18n.l backlog.created_at, :format => :short, :locale => backlog.account.locale.code.to_sym
+    I18n.l backlog.updated_at, :format => :short, :locale => backlog.account.locale.code.to_sym
   end
 
   # if JSON is embedded in CDATA Javascript and there is a value of </script> it closes the opening <script> tag incorrectly
