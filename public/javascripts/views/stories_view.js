9e0f637dd4279390ea4472bbb028ec962912c7bf
Matthew O'Riordan <matthew.oriordan@gmail.com>
2011-11-07 17:46:10 +0000

    Color setting to white bug due to transparency

--- public/javascripts/views/stories_view.js
+++ public/javascripts/views/stories_view.js
@@ -469,9 +469,13 @@ App.Views.Stories = {
     changeColor: function(color, options) {
       var colorWithoutHex = (color.match(/^#/) ? color.substring(1) : color);
       var colorWithHex = '#' + colorWithoutHex;
-      if (colorWithoutHex.toLowerCase() === 'ffffff') { colorWithoutHex = colorWithHex = ''; }
-      var colors = colorWithoutHex.match(/[\d\w]{2}/g);
-      $(this.el).css('background-color', 'rgba(' + parseInt(colors[0],16) + ', ' + parseInt(colors[1],16) + ', ' + parseInt(colors[2],16) + ', 0.15)');
+      if (colorWithoutHex.toLowerCase() === 'ffffff') {
+        colorWithoutHex = colorWithHex = '';
+        $(this.el).css('background-color','transparent');
+      } else {
+        var colors = colorWithoutHex.match(/[\d\w]{2}/g);
+        $(this.el).css('background-color', 'rgba(' + parseInt(colors[0],16) + ', ' + parseInt(colors[1],16) + ', ' + parseInt(colors[2],16) + ', 0.15)');
+      }
       $(this.el).find('.background-color-indicator').css('background-color', colorWithHex);
       if (!options || !options.silent) {
         this.model.set({ color: colorWithoutHex });
