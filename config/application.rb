5bb88583c5d8e70bb63c1d394f638318f32c1d9d
Matthew O'Riordan <matthew.oriordan@gmail.com>
2012-01-18 18:55:16 +0000

    Default language English (try fix locale issue)

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module Ibacklog
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', 'framework', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
