3c0a7cdd64657771e3543e3e9d0beb05a3faff05
Matthew O'Riordan <matthew.oriordan@gmail.com>
2012-02-16 06:45:42 +0000

    Incorrectly deleting permissions when adding

--- db/migrate/20120209123858_add_backlog_users.rb
+++ db/migrate/20120209123858_add_backlog_users.rb
@@ -10,9 +10,11 @@ class AddBacklogUsers < ActiveRecord::Migration
     add_index :backlog_users, :user_id
     add_index :backlog_users, :backlog_id
 
+    # clear any backlog_users that may exist
+    execute 'delete from backlog_users'
+
     # now assign full rights to every user for the Example backlog as by default they will get access to this in future
     Backlog.masters.where('name ilike ?', 'example corporate website backlog').each do |backlog|
-      execute 'delete from backlog_users'
       backlog.account.account_users.each do |account_user|
         execute "insert into backlog_users (backlog_id, user_id, privilege, created_at, updated_at) VALUES (#{backlog.id}, #{account_user.user_id}, 'full', now(), now())"
       end
