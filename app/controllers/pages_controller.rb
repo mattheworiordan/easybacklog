7a3fa96ed34f5056b48a3ec05e001a07708de513
Matthew O'Riordan <matthew.oriordan@gmail.com>
2012-04-07 18:38:38 +0200

    Require login for dashboard page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,19 @@
 class PagesController < ApplicationController
   basic_allowed :contact, :browser_support
+  before_filter :authenticate_user!, :except => [:raise_error, :status]
 
   # routed as dashboard
   def home
-    if user_signed_in?
-      if current_user.accounts.length > 1
-        # show activity across all accounts
-        @recent_backlogs = current_user.accounts.map { |accounts| accounts.backlogs.active.where_user_has_access(current_user) }
-        # show most recent first
-        @recent_backlogs = @recent_backlogs.flatten.sort_by(&:updated_at).reverse[0..15]
-        # group by company/account
-        @recent_backlogs = @recent_backlogs.group_by { |backlog| backlog.company.present? ? backlog.company : backlog.account }
-      else
-        flash.keep
-        redirect_to logged_in_user_home_path
-      end
+    if current_user.accounts.length > 1
+      # show activity across all accounts
+      @recent_backlogs = current_user.accounts.map { |accounts| accounts.backlogs.active.where_user_has_access(current_user) }
+      # show most recent first
+      @recent_backlogs = @recent_backlogs.flatten.sort_by(&:updated_at).reverse[0..15]
+      # group by company/account
+      @recent_backlogs = @recent_backlogs.group_by { |backlog| backlog.company.present? ? backlog.company : backlog.account }
+    else
+      flash.keep
+      redirect_to logged_in_user_home_path
     end
   end
 
