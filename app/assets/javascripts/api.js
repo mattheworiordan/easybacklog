b6a3c13d390e650b128edce00edfb606fb7258ca
Matthew O'Riordan <matthew.oriordan@gmail.com>
2012-05-25 19:12:07 +0200

    Added user echo feedback to API

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -3,8 +3,25 @@
 //= require conditional/syntax-highlighter/shCore
 //= require conditional/syntax-highlighter/shBrushJScript
 //= require conditional/syntax-highlighter/shBrushXml
+//= require shared/user_echo
 
 $(function() {
   SyntaxHighlighter.defaults['gutter'] = false;
   SyntaxHighlighter.all();
 });
+
+// enable feedback button
+setTimeout(function() {
+  $('a.feedback-button').mouseover(function() {
+    if (typeof UE !== 'undefined') {
+      UE.Popin.preload();
+    }
+  }).click(function(event) {
+    event.preventDefault();
+    if (typeof UE !== 'undefined') {
+      UE.Popin.show();
+    } else {
+      document.location.href = '/contact';
+    }
+  }).fadeIn();
+}, 1500);
--- app/assets/javascripts/shared/global.js
+++ app/assets/javascripts/shared/global.js
@@ -121,23 +121,6 @@ $(document).ready(function() {
     _.delay(function() { alertNotice.slideUp(); }, 5000);
   });
 
-  if (App.environment !== 'test') {
-    /* Standard UserEcho link code */
-    _ues = {
-      host:'easybacklog.userecho.com',
-      forum:'4890',
-      lang:'en',
-      tab_show:false
-    };
-    (function() {
-        var s, _ue = document.createElement('script');
-        _ue.type = 'text/javascript';
-        _ue.async = true;
-        _ue.src = ('https:' === document.location.protocol ? 'https://s3.amazonaws.com/' : 'http://') + 'cdn.userecho.com/js/widget-1.4.gz.js';
-        s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(_ue, s);
-    }());
-  }
-
   // enable feedback button
   setTimeout(function() {
     $('a.feedback-button').mouseover(function() {
