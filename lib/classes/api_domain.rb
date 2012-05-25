646c8d7fdc8ec5a723dd6d55c4644c4c18ae90cc
Matthew O'Riordan <matthew.oriordan@gmail.com>
2012-05-25 18:09:25 +0200

    domain bug not checking full host name for API match

--- lib/classes/api_domain.rb
+++ lib/classes/api_domain.rb
@@ -1,6 +1,6 @@
 # API domain matcher, returns true if request looks like it is for an API domain
 class ApiDomain
   def self.matches?(request)
-    (request.subdomain.present? && request.subdomain == "api") || request.domain =~ /^api\-easybacklog\-\w+\.heroku(?:|app).com$/ || request.domain == 'api-local' || request.headers['X-Forward-To-API'] == 'true'
+    (request.subdomain.present? && request.subdomain == "api") || request.host =~ /^api\-easybacklog\-\w+\.heroku(?:|app).com$/ || request.domain == 'api-local' || request.headers['X-Forward-To-API'] == 'true'
   end
 end
