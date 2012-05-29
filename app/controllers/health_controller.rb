2d98e3e7997e70d95fec0312b758a4e75b629ec0
Matthew O'Riordan <matthew.oriordan@gmail.com>
2012-05-29 23:32:02 +0200

    Health check bug - incorrect header

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -2,7 +2,7 @@ class HealthController < ActionController::Base
   def status
     begin
       raise 'No users in the database' if User.count == 0
-      render :text => "easyBacklog appears to be healthy", :status => 500, :content_type => "text/plain"
+      render :text => "easyBacklog appears to be healthy", :content_type => "text/plain"
     rescue Exception => e
       render :text => "There's a big problem, help. #{e.message}", :status => 500, :content_type => "text/plain"
     end
