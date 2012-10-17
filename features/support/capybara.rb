96ffbc0b231cd2e2a604f67f60b0f5bfe8044b2a
Matthew O'Riordan <matthew.oriordan@gmail.com>
2012-10-17 22:22:25 +0200

    Be a bit more lenient when running integration tests to reduce erroneous errors

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -1,2 +1,3 @@
 Capybara.save_and_open_page_path = 'tmp/capybara'
 Capybara.javascript_driver = :selenium
+Capybara.default_wait_time = 4 # double the wait time as we've experienced intermitten problems
