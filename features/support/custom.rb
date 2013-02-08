cbcde15a4d1c8f3631a98a8aea99d74563e4eb0e
Matthew O'Riordan <matthew.oriordan@gmail.com>
2013-02-08 15:48:19 +0000

    Improve test speed by using transactions instead of truncation for Cucumber

--- features/support/custom.rb
+++ features/support/custom.rb
@@ -16,3 +16,17 @@ Before('@javascript,@selenium') do
     Capybara.current_session.driver.resize_window 1000,720
   end
 end
+
+# allow the use of transactions even with Cucumber
+class ActiveRecord::Base
+  mattr_accessor :shared_connection
+  @@shared_connection = nil
+
+  def self.connection
+    @@shared_connection || retrieve_connection
+  end
+end
+
+# Forces all threads to share the same connection. This works on
+# Capybara because it starts the web server in a thread.
+ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
