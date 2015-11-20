20d91d3bb62c0fbafbf7145a07351cefa4b58fd8
Marc Roberts <marc@neutroncreations.com>
2015-11-20 13:06:52 +0000

    use first match
    capybara now throws an exception if there are duplicate matches unless you explicitly ask for only the first

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -32,7 +32,7 @@ end
 
 When /^(?:|I )fill in "([^"]*)" with "([^"]*)"(?: within "([^"]*)")?$/ do |field, value, selector|
   with_scope(selector) do
-    fill_in(field, :with => value)
+    fill_in(field, :with => value, match: :first)
   end
 end
 
